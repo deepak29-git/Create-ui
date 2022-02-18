c16df7a601c54412a5a3a20c3eb0bb976a60082e
Deepak Goyal <dg887180@gmail.com>
2022-02-18 16:39:52 +0530

    fix: fix hamburger bug

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 
 const hamburger=document.querySelector("#hamburger");
 const navMenu=document.querySelector(".nav-menu");
-console.log(navMenu);
+
 hamburger.addEventListener("click",()=>{
-    if(navMenu.style.left === "-100%"){
-        navMenu.style.left = "0";
-        hamburger.innerText="close";
-    }else{
-        navMenu.style.left = "-100%"
+    if(navMenu.style.left === "0px"){
+        navMenu.style.left = "-100%";
         hamburger.innerText="menu";
-     
-    }
-    
+    }else{
+        navMenu.style.left = "0px"
+        hamburger.innerText="close";
+    } 
 })
