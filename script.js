55c4dc9bf0f613702cccccbb8819be0e0c36aff9
Deepak Goyal <dg887180@gmail.com>
2022-02-21 18:56:26 +0530

    style: update color

--- script.js
+++ script.js
@@ -17,12 +17,12 @@ const navMenu=document.querySelector(".nav-menu");
 
 darkMode.addEventListener("click",()=>{
     if(body.style.backgroundColor==="var(--dark-color)"){
-        body.style.backgroundColor="var(--white-color)"
+        body.style.backgroundColor="white"
         body.style.color="var(--dark-color)"
         darkMode.innerText="dark_mode"
-        header.style.backgroundColor="var(--white-color)"
+        header.style.backgroundColor="white"
         logoTitle.style.color="var(--dark-color)"
-        listTitle.style.color="var(--white-color)"
+        listTitle.style.color="white"
         homeIcon.style.color="var(--dark-color)"
         for(let i=0; i<showComponent.length;i++){
             showComponent[i].style.backgroundColor="var(--accent-color)"
@@ -33,17 +33,17 @@ darkMode.addEventListener("click",()=>{
         
     }else{
         body.style.backgroundColor="var(--dark-color)"
-        body.style.color="var(--white-color)"
+        body.style.color="white"
         darkMode.innerText="light_mode"
         header.style.backgroundColor="var(--dark-color)"
-        logoTitle.style.color="var(--white-color)"
-        homeIcon.style.color="var(--white-color)"
+        logoTitle.style.color="white"
+        homeIcon.style.color="white"
         listTitle.style.color="var(--dark-color)"
         for(let i=0; i<showComponent.length;i++){
             showComponent[i].style.backgroundColor="var(--show-component-dark)"
         }
         for(let i=0;i<componentLink.length; i++){
-            componentLink[i].style.color="var(--white-color)"
+            componentLink[i].style.color="white"
 
         }
         
