9e5136bf603be205059ddfaa1f234f13283ff434
Deepak Goyal <dg887180@gmail.com>
2022-02-21 17:04:30 +0530

    fix: fix dark mode bug

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 
-const hamburger=document.querySelector("#hamburger");
-const navMenu=document.querySelector(".nav-menu");
-
-
-hamburger.addEventListener("click",()=>{
-    if(navMenu.style.left === "0px"){
-        navMenu.style.left = "-100%";
-        hamburger.innerText="menu";
-    }else{
-        navMenu.style.left = "0px"
-        hamburger.innerText="close";
-    } 
-})
-
 
 
 // dark mode
@@ -60,5 +46,20 @@ darkMode.addEventListener("click",()=>{
         }
         
     }
+})
+
+
+const hamburger=document.querySelector("#hamburger");
+const navMenu=document.querySelector(".nav-menu");
+
+
+hamburger.addEventListener("click",()=>{
+    if(navMenu.style.left === "0px"){
+        navMenu.style.left = "-100%";
+        hamburger.innerText="menu";
+    }else{
+        navMenu.style.left = "0px"
+        hamburger.innerText="close";
+    } 
 })
 
